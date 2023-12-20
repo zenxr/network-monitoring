4eca5af7f2b019085bcdaa8c49922396a3968fff
Cody Stephenson <cdystphnsn@gmail.com>
2023-12-20 14:07:47 -0700

    Minor cleanup, add logging

--- monitor/bandwidth.sh
+++ monitor/bandwidth.sh
@@ -9,17 +9,20 @@ run_speedtest() {
 parse_result() {
     local result="$(cat)"
     local strip_to_int='s/([0-9])\..*/\1/'
+    local latency_ms
+    local download_bps
+    local upload_bps
 
-    local latency_ms="$(echo "$result" | cut -d ',' -f 7 | sed -r "$strip_to_int")"
-    local download_bits_sec="$(echo "$result" | cut -d ',' -f 8 | sed -r "$strip_to_int")"
-    local upload_bits_sec="$(echo "$result" | cut -d ',' -f 9 | sed -r "$strip_to_int")"
+    latency_ms="$(echo "$result" | cut -d ',' -f 7 | sed -r "$strip_to_int")"
+    download_bps="$(echo "$result" | cut -d ',' -f 8 | sed -r "$strip_to_int")"
+    upload_bps="$(echo "$result" | cut -d ',' -f 9 | sed -r "$strip_to_int")"
+    say_info "Latency: $latency_ms, Download bps: $download_bps, Upload bps: $upload_bps"
 
-    echo $latency_ms $(( download_bits_sec / 1024 )) $(( upload_bits_sec / 1024 ))
+    echo $latency_ms $(( download_bps / 1024 )) $(( upload_bps / 1024 ))
 }
 
 store_result() {
     local input="$(cat)"
-    echo input is "$input"
 
     local latency
     local download
@@ -28,21 +31,32 @@ store_result() {
     read -r latency download upload <<< "$input"
 
     local sql="insert into bandwidth_metric (source, latency_ms, download_mbs, upload_mbs) values ('speedtest', $latency, $download, $upload)"
+    say_info "$sql"
     psql -c "$sql"
 }
 
 main() {
+    say_info "Running bandwidth monitoring..."
     local output=
     local status=0
 
     output="$(run_speedtest)" || status=$?
 
     if [ "$status" -ne 0 ]; then
+        say_err "Speedtest failed, storing failure..."
         echo "null null null" | store_result
         exit 1
     fi
 
     echo "$output" | parse_result | store_result
+}
+
+say_info() {
+  printf '%s\n' "$@" >&2
+}
+
+say_err() {
+  echo "ERROR: $*" >&2
 }
 
 main "$@"
